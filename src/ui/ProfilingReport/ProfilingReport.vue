5ddc6e75675694aed497f02a5999c65bc6e34f25
Anatoly.Nikitin <Anatoly.Nikitin@jetbrains.com>
2026-08-20 17:52:48 +0300

    Animate toolbar zoom-to-fit with the same view-edge easing
    Reuse the Δt focus tween so reset zoom eases to the full trace instead of jumping.

--- src/ui/ProfilingReport/ProfilingReport.vue
+++ src/ui/ProfilingReport/ProfilingReport.vue
@@ -179,13 +179,14 @@ function animateToWindow(window: { startTime: number; endTime: number; scrollY: 
     startTime: viewState.value.startTime,
     endTime: viewState.value.endTime,
   };
+  const scrollY = window.scrollY;
   cancelViewWindowAnim = animateViewWindow({
     from,
     to: { startTime: window.startTime, endTime: window.endTime },
     onUpdate: (w) => {
       viewState.value = applyWindow(viewState.value, {
         ...w,
-        scrollY: viewState.value.scrollY,
+        scrollY,
       });
     },
     onDone: () => {
@@ -412,8 +413,7 @@ function onZoom(factor: number, anchorTime: number) {
 }
 
 function onZoomToFit() {
-  stopViewWindowAnim();
-  viewState.value = applyWindow(viewState.value, zoomToFitWindow(swim.value));
+  animateToWindow(zoomToFitWindow(swim.value));
 }
 
 function onZoomIn() {
